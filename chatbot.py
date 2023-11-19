fa50e5beb7f5cce65927477efd2f6edca6b57c6a
Gamma Ray <145352304+Xelionix@users.noreply.github.com>
2023-11-19 02:56:02 -0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request, jsonify
-from transformers import pipeline
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
 app = Flask(__name__)
 
-# Load the conversational pipeline from transformers
-chatbot = pipeline("conversational")
+# Load GPT-Neo 2.7B model and tokenizer
+model = GPT2LMHeadModel.from_pretrained("DB13067/Peterbot")
+tokenizer = GPT2Tokenizer.from_pretrained("EleutherAI/gpt-neo-2.7B")
 
 @app.route('/')
 def home():
@@ -14,8 +15,10 @@ def home():
 def get_response():
     user_input = request.form['user_input']
 
-    # Use the chatbot for conversational responses
-    chatbot_response = chatbot(user_input)[0]["generated_responses"][0]
+    # Tokenize user input and generate response
+    input_ids = tokenizer.encode(user_input, return_tensors="pt")
+    chatbot_response_ids = model.generate(input_ids, max_length=100, num_return_sequences=1)
+    chatbot_response = tokenizer.decode(chatbot_response_ids[0], skip_special_tokens=True)
 
     return jsonify({'response': chatbot_response})
 
